be90d9dbc28b8d15e0aa37d85e6fd1cad95e7abc
Aide Sarahi <aideflores5874@alumnos.udg.mx>
2020-09-29 12:35:51 -0500

    prueba 01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string tipo;
+    
+    getline (cin,tipo);
+
+    if (tipo == "char"){
+        cout<<sizeof(char)<<endl;
+    }
+    else if(tipo == "int"){
+        cout << sizeof(int)<<endl;
+    }
+    else if(tipo == "short"){
+        cout << sizeof(short)<<endl;
+    }
+    else if(tipo == "float"){
+        cout << sizeof(float)<<endl;
+    }
+    else if(tipo == "double"){
+        cout << sizeof(double)<<endl;
+    }
+    else if(tipo == "long"){
+        cout << sizeof(long)<<endl;
+    }
 
     return 0;
 }
